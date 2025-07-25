857b5ffbf892cee028f8fd2fc0f9f3650bc0927e
202504_xuanb <tomato.202504.xuanb@gmail.com>
2025-07-25 11:42:57 +0900

    7/25 コードはまだ続いています。

--- Java/PuzzleGame/src/GameJFrame.java
+++ Java/PuzzleGame/src/GameJFrame.java
@@ -1,30 +1,30 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.WindowConstants;
+import javax.swing.*;
 import javax.swing.border.BevelBorder;
 
-public class GameJFrame extends JFrame {
+public class GameJFrame extends JFrame implements KeyListener {
+
   int[][] data = new int[4][4];
+  int x = 0;
+  int y = 0;
+  String path = "image\\animal\\animal1\\";
+  int[][] win = {
+      {1, 2, 3, 4},
+      {5, 6, 7, 8},
+      {9, 10, 11, 12},
+      {13, 14, 15, 0}
+  };
 
-  public GameJFrame(){
-    //ヴィンドウ初期化
+  JLabel[][] labels = new JLabel[4][4];
+
+  public GameJFrame() {
     initJFrame();
-
-    //メニュー初期化
     initJMenuBar();
-
-    //画像位置初期化(Random)
     initData();
-
-    //画像初期化
-    initImage();
-
-    //ウィンドウを表示する
+    initImageFirstTime();
+    updateImages();
     this.setVisible(true);
   }
 
@@ -34,76 +34,144 @@ public class GameJFrame extends JFrame {
     Random random = new Random();
     for (int i = 0; i < tempArr.length; i++) {
       int index = random.nextInt(tempArr.length);
-      int temp;
-      temp = tempArr[i];
+      int temp = tempArr[i];
       tempArr[i] = tempArr[index];
       tempArr[index] = temp;
     }
 
     for (int i = 0; i < tempArr.length; i++) {
       data[i / 4][i % 4] = tempArr[i];
+      if (tempArr[i] == 0) {
+        x = i / 4;
+        y = i % 4;
+      }
     }
   }
 
-  private void initImage() {
+  private void initImageFirstTime() {
+    this.getContentPane().setLayout(null);
 
     for (int i = 0; i < 4; i++) {
       for (int j = 0; j < 4; j++) {
-        int number = data[i][j];
-        //JLabel のオブジェクトを作成する
-        JLabel jLabel = new JLabel(new ImageIcon("image\\animal\\animal1\\" + number + ".jpg"));
-        jLabel.setBorder(new BevelBorder(BevelBorder.LOWERED));
-
-        //位置とサイズを設定する
-        jLabel.setBounds(105 * j + 83, 105 * i + 134, 105, 105);
-
-        //画面に追加する
-        this.getContentPane().add(jLabel);
+        labels[i][j] = new JLabel();
+        labels[i][j].setBounds(105 * j + 83, 105 * i + 134, 105, 105);
+        labels[i][j].setBorder(new BevelBorder(BevelBorder.LOWERED));
+        this.getContentPane().add(labels[i][j]);
       }
     }
 
-    //背景画像追加
+    // 背景图（最后添加）
     JLabel background = new JLabel(new ImageIcon("image\\background.png"));
     background.setBounds(40, 40, 508, 560);
     this.getContentPane().add(background);
   }
 
-  private void initJFrame() {
-    //ウィンドウのサイズを設定する
-    this.setSize(603, 680);
+  private void updateImages() {
+    for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < 4; j++) {
+        int number = data[i][j];
+        if (number != 0) {
+          labels[i][j].setIcon(new ImageIcon(path + number + ".jpg"));
+        } else {
+          labels[i][j].setIcon(null);
+        }
+      }
+    }
 
-    //ウィンドウのタイトルを設定する
-    this.setTitle("パズルゲーム");
+    this.getContentPane().repaint();
 
-    //常に最前面に表示するように設定する
-    this.setAlwaysOnTop(true);
-
-    //画面中央にウィンドウを表示する
-    this.setLocationRelativeTo(null);
-
-    //ウィンドウを閉じたときにプログラムを終了するように設定する
-    this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-    //中央揃えを無効にする
-    this.setLayout(null);
+    if (victory()) {
+      JOptionPane.showMessageDialog(this, "🎉 勝利！");
+    }
   }
 
+  private void initJFrame() {
+    this.setSize(603, 680);
+    this.setTitle("パズルゲーム");
+    this.setAlwaysOnTop(true);
+    this.setLocationRelativeTo(null);
+    this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    this.setLayout(null);
+    this.addKeyListener(this);
+
+  }
 
   private void initJMenuBar() {
     JMenuBar jMenuBar = new JMenuBar();
-
     JMenu functhionJMenu = new JMenu("機能");
 
     JMenuItem rePlayItem = new JMenuItem("再プレイ");
     JMenuItem reLoginItem = new JMenuItem("再ログイン");
     JMenuItem closeItem = new JMenuItem("終了");
 
+    rePlayItem.addActionListener(e -> {
+      initData();
+      updateImages();
+    });
+    closeItem.addActionListener(e -> System.exit(0));
+
     functhionJMenu.add(rePlayItem);
     functhionJMenu.add(reLoginItem);
     functhionJMenu.add(closeItem);
-
     jMenuBar.add(functhionJMenu);
 
     this.setJMenuBar(jMenuBar);
+  }
+
+  @Override
+  public void keyTyped(KeyEvent e) {}
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+    if (e.getKeyCode() == KeyEvent.VK_A) {
+      JLabel all = new JLabel(new ImageIcon(path + "all.jpg"));
+      all.setBounds(83, 134, 420, 420);
+      this.getContentPane().add(all);
+      this.getContentPane().repaint();
+    }
+  }
+
+  @Override
+  public void keyReleased(KeyEvent e) {
+    if (victory()) return;
+
+    int code = e.getKeyCode();
+    if (code == KeyEvent.VK_LEFT && y > 0) {
+      data[x][y] = data[x][y - 1];
+      data[x][y - 1] = 0;
+      y--;
+    } else if (code == KeyEvent.VK_RIGHT && y < 3) {
+      data[x][y] = data[x][y + 1];
+      data[x][y + 1] = 0;
+      y++;
+    } else if (code == KeyEvent.VK_UP && x > 0) {
+      data[x][y] = data[x - 1][y];
+      data[x - 1][y] = 0;
+      x--;
+    } else if (code == KeyEvent.VK_DOWN && x < 3) {
+      data[x][y] = data[x + 1][y];
+      data[x + 1][y] = 0;
+      x++;
+    } else if (code == KeyEvent.VK_A) {
+      updateImages();
+    } else if (code == KeyEvent.VK_W) {
+      data = new int[][]{
+          {1, 2, 3, 4},
+          {5, 6, 7, 8},
+          {9, 10, 11, 12},
+          {13, 14, 15, 0}
+      };
+      x = 3;
+      y = 3;
+    }
+
+    updateImages();
+  }
+
+  public boolean victory() {
+    for (int i = 0; i < 4; i++)
+      for (int j = 0; j < 4; j++)
+        if (data[i][j] != win[i][j]) return false;
+    return true;
   }
 }
